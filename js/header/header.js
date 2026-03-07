352849484dcf6f4ace90b0ef646e15e40e11c2f0
Iñaki <tatigop09@gmail.com>
2026-03-07 16:24:37 -0300

    add responsive design for tablets and desktop M (1024px) without the footer adaptation

--- js/header/header.js
+++ js/header/header.js
@@ -4,16 +4,20 @@ const logo = document.querySelector(".logo-container");
 const hamburgerMenu = document.querySelector(".hamburger-menu-icon");
 const mobileMenu = document.querySelector(".mobile-menu");
 
+const desktopMenu = document.querySelector(".desktop-menu");
+
 
 window.addEventListener("scroll", () => {
     if (window.scrollY > 100) {
         header.classList.add("scrolled");
         hamburgerMenu.classList.add("scrolled");
         logo.classList.add("scrolled");
+        desktopMenu.classList.add("scrolled");
     } else {
         header.classList.remove("scrolled");
         hamburgerMenu.classList.remove("scrolled");
         logo.classList.remove("scrolled");
+        desktopMenu.classList.remove("scrolled");
         mobileMenu.classList.remove("open");
     }
 });
