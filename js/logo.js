e5b7206634e00f626557c9040772a7bb9b25ae73
Iñaki <tatigop09@gmail.com>
2026-03-12 18:24:29 -0300

    change logo, fix the logo animation, refactor hero and add more text to the about section

--- js/logo.js
+++ js/logo.js
@@ -1,11 +1,14 @@
 const logo = document.querySelector(".logo-container");
+const logoImage = document.getElementById("muste-icon-img")
 
 window.addEventListener("scroll", () => {
 
     //If the user scrolls down
     if (window.scrollY > 10) {
         logo.classList.add("scrolled");
+        logoImage.src = "assets/logo-muste-white.png";
     } else {
         logo.classList.remove("scrolled");
+        logoImage.src = "assets/logo-muste.png";
     }
 });
