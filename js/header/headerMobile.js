022a4362ab7a678045fbf7e8ac2b538e7b5de227
Iñaki <tatigop09@gmail.com>
2026-03-14 16:36:17 -0300

    add plan section and fix UI details

--- js/header/headerMobile.js
+++ js/header/headerMobile.js
@@ -1,6 +1,9 @@
 const hamburgerMenu = document.querySelector(".hamburger-menu-icon");
 const mobileMenu = document.querySelector(".mobile-menu");
-const mobileMenuBtns = document.querySelectorAll(".mobile-menu-btn")
+const mobileMenuBtns = document.querySelectorAll(".mobile-menu-btn");
+
+const hamburgerIcon = document.getElementById("hamburger-menu");
+const hamburgerIconCross = document.getElementById("hamburger-menu-cross");
 
 window.addEventListener("scroll", () => {
 
@@ -15,10 +18,13 @@ window.addEventListener("scroll", () => {
 
 hamburgerMenu.addEventListener("click", () => {
     mobileMenu.classList.toggle("open");
-    
+    hamburgerIcon.classList.toggle("hidden");
+    hamburgerIconCross.classList.toggle("hidden");
 });
 
 mobileMenuBtns.forEach( btn => btn.addEventListener("click", () => {
     mobileMenu.classList.remove("open");
+    hamburgerIcon.classList.remove("hidden");
+    hamburgerIconCross.classList.add("hidden");
 }))
 
